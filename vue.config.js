dec598a146c7d76c281b57e84ffe5556a26944bd
naremloa <naremloa@gmail.com>
2019-02-14 15:18:38 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/v1/api': {
+        target: 'http://localhost:5000/',
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    // config
+    //   .plugin('html')
+    //   .tap((args) => {
+    //     args[0].chunksSortMode = 'none';
+    //     return args;
+    //   });
+    config
+      .plugin('define')
+      .tap((args) => {
+        // if (args[0]['process.env'].VUE_APP_MODE_ENV !== '"PRODUCTION"') {
+        //   args[0]['process.env'].BUILD_TIME = `${Date.now()}`;
+        // }
+        args[0]['process.env'].BUILD_TIME = `${Date.now()}`;
+        return args;
+      });
+    config
+      .devServer.port(8080);
+  },
+};
