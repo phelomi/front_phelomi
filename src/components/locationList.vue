a9a70309f8119cb2683d131a57044715411ee7a7
lucy <qwer558a1@gmail.com>
2019-02-14 14:37:13 +0800

    update

---
+++ src/components/locationList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="location-list">
+    <v-layout row>
+      <v-flex xs8 class="location-list__left">
+        <div class="location-list__title text-under-line">
+          <div class="location-list__title--img">
+            <img :src="titleIcon" alt="">
+          </div>
+          <h2 class="primary--text">{{title}}</h2>
+        </div>
+        <div class="location-list__address">
+          <p class="primary--text">{{targetTitle}}</p>
+          <h3 class="error--text">{{targetAddress}}</h3>
+        </div>
+        <div class="location-list__traffic">
+          <h3 class="primary--text location-list__traffic--title">{{trafficTitle}}</h3>
+          <div
+            v-for="(item, idx) in trafficList"
+            :key="`trafficList${idx}`"
+            class="location-list__traffic-list"
+          >
+            <div class="primary--text location-list__traffic-list--row">
+              <div class="location-list__traffic-list--target">{{item.target}}</div>
+              <div class="location-list__traffic-list--describe">{{item.describe}}</div>
+              <div class="location-list__traffic-list--number white--text error">{{item.number}}</div>
+              <div class="location-list__traffic-list--unit">{{item.unit}}</div>
+            </div>
+          </div>
+        </div>
+      </v-flex>
+      <v-flex xs4 class="location-list__right">
+        <div class="location-list__img">
+          <img :src="image" alt="">
+        </div>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+<script>
+export default {
+  name: 'locationList',
+  props: [
+    'title',
+    'titleIcon',
+    'targetTitle',
+    'targetAddress',
+    'trafficTitle',
+    'trafficList',
+    'image',
+  ],
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
--- src/views/pages/Location.vue
+++ src/views/pages/Location.vue
@@ -9,23 +9,38 @@
         />
       </v-flex>
     </v-layout>
-    <div class="primary-content page-location__content">
-      <v-layout row>
-        <v-flex xs9>
-        </v-flex>
-        <v-flex xs3>
-        </v-flex>
-      </v-layout>
+    <div class="page-location__content">
+      <location-list
+        v-for="(item, idx) in locationList"
+        :key="`locationList${idx}`"
+        :title = item.title
+        :titleIcon = item.titleIconSrc
+        :targetTitle = item.targetTitle
+        :targetAddress = item.targetAddress
+        :trafficTitle = item.trafficTitle
+        :trafficList = item.trafficList
+        :image = item.imageSrc
+      />
     </div>
   </div>
 </template>
 <script>
 import titleBoat from '@/components/titleBoat.vue';
+import locationList from '@/components/locationList.vue';
+import iconPosition from '../../assets/location/icon_position.svg';
+import iconAirplane from '../../assets/location/icon_airplane.svg';
+import iconShip from '../../assets/location/icon_ship.svg';
+
+import mapPosition from '../../assets/location/map_position.png';
+import mapAirplane from '../../assets/location/map_airplane.png';
+import mapShip from '../../assets/location/map_ship.png';
+
 
 export default {
   name: 'pageLocation',
   components: {
     titleBoat,
+    locationList,
   },
   data() {
     return {
@@ -34,9 +49,74 @@ export default {
         textDown: 'LOCATION',
         targetClass: 'target-location',
       },
-      info: {
-
-      },
+      locationList: [
+        {
+          title: '民宿地理位置',
+          titleIconSrc: iconPosition,
+          targetTitle: '飛樂米星空號位於',
+          targetAddress: '澎湖縣馬公市山水里22鄰珠江182號',
+          trafficTitle: '民宿距離',
+          trafficList: [
+            {
+              target: '馬公機場', describe: '車程約', number: '15～30', unit: '分鐘',
+            },
+            {
+              target: '馬公港口', describe: '車程約', number: '30', unit: '分鐘',
+            },
+            {
+              target: '馬公市區', describe: '車程約', number: '20', unit: '分鐘',
+            },
+            {
+              target: '山水沙灘', describe: '車程約', number: '10', unit: '分鐘',
+            },
+          ],
+          imageSrc: mapPosition,
+        },
+        {
+          title: '搭船',
+          titleIconSrc: iconAirplane,
+          targetTitle: '馬公碼頭位於',
+          targetAddress: '澎湖縣馬公市新營路25號',
+          trafficTitle: '船班資訊',
+          trafficList: [
+            {
+              target: '台華輪', describe: '航程約', number: '80', unit: '分鐘',
+            },
+            {
+              target: '滿天星號', describe: '航程約', number: '80', unit: '分鐘',
+            },
+            {
+              target: '凱旋三號', describe: '航程約', number: '85', unit: '分鐘',
+            },
+            {
+              target: '凱旋六號', describe: '航程約', number: '85', unit: '分鐘',
+            },
+          ],
+          imageSrc: mapAirplane,
+        },
+        {
+          title: '搭飛機',
+          titleIconSrc: iconShip,
+          targetTitle: '澎湖機場位於',
+          targetAddress: '澎湖縣湖西鄉隘門村126-5號',
+          trafficTitle: '班機資訊',
+          trafficList: [
+            {
+              target: '立榮航空', describe: '航程約', number: '30～50', unit: '分鐘',
+            },
+            {
+              target: '華信航空', describe: '航程約', number: '30～50', unit: '分鐘',
+            },
+            {
+              target: '德安航空', describe: '航程約', number: '30～50', unit: '分鐘',
+            },
+            {
+              target: '遠東航空', describe: '航程約', number: '30～50', unit: '分鐘',
+            },
+          ],
+          imageSrc: mapShip,
+        },
+      ],
     };
   },
   methods: {
