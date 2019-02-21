838af27cbc4fdc38eddd7b9ff16b776af76e163b
lucy <qwer558a1@gmail.com>
2019-02-21 18:40:25 +0800

    update regex

--- src/permission.js
+++ src/permission.js
@@ -2,7 +2,7 @@ import router from './router';
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title ? `${to.meta.title}-飛樂米星空號` : '飛樂米星空號';
-  if (!/^\/maintenance/.test(to.path)) {
+  if (!/^\/maintenance(?:\/(?=$))?$/i.test(to.path)) {
     next('/maintenance');
   }
   next();
