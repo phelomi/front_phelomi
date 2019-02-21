f61ecfcee9543d2ebe79ba723290ca70319e1001
lucy <qwer558a1@gmail.com>
2019-02-21 18:43:15 +0800

    update

--- src/permission.js
+++ src/permission.js
@@ -2,8 +2,8 @@ import router from './router';
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title ? `${to.meta.title}-飛樂米星空號` : '飛樂米星空號';
-  if (!/^\/maintenance(?:\/(?=$))?$/i.test(to.path)) {
-    next('/maintenance');
-  }
+  // if (!/^\/maintenance(?:\/(?=$))?$/i.test(to.path)) {
+  //   next('/maintenance');
+  // }
   next();
 });
