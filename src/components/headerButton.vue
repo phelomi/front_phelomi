1bea7d702e57d5bbd14e6395805e4a1a0e270940
lucy <qwer558a1@gmail.com>
2019-03-07 19:42:10 +0800

    歷史性的一刻，大更新了header，還有更新了 跟我醬玩變成圖片了！

--- src/components/headerButton.vue
+++ src/components/headerButton.vue
@@ -3,17 +3,10 @@
     class="header__button"
     @click="methodGoTo"
   >
-    <span class="header__button__head"></span>
-    <div class="header__button__body">
-      <img :src="buttonBodyFront" alt="" class="header__button__body__front">
-      <div class="header__button__body__text"><span>{{this.text}}</span></div>
-      <img :src="buttonBodyEnd" alt="" class="header__button__body__end">
-    </div>
+    <div class="header__button--text white--text">{{this.text}}</div>
   </a>
 </template>
 <script>
-import buttonBodyFront from '../assets/button/button_header_body_front.svg';
-import buttonBodyEnd from '../assets/button/button_header_body_end.svg';
 import constVar from '@/utils/constVar';
 
 export default {
@@ -21,17 +14,12 @@ export default {
   props: ['text', 'target', 'directToPage'],
   data() {
     return {
-      buttonBodyFront,
-      buttonBodyEnd,
       localText: '',
     };
   },
   computed: {
     currentURL() {
       return this.$route.path;
-    },
-    buttonLg() {
-      return this.text.length > 4 ? 'header__button__lg' : '';
     },
   },
   methods: {
