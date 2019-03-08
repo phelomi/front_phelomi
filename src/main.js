04b000b64b08a84caa1ae12955c418060ffbf19c
lucy <qwer558a1@gmail.com>
2019-03-08 14:18:24 +0800

    close ServiceWorker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import './registerServiceWorker';
+// import './registerServiceWorker';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import '@/theme/index.styl';
