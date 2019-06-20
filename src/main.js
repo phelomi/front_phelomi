477efaec5a1096ebede48e59f99e5457ecbd488b
naremloa <naremloa@gmail.com>
2019-06-20 23:52:46 +0800

    Signed-off-by: naremloa <naremloa@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 // import './registerServiceWorker';
+// import
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
 import '@/theme/index.styl';
